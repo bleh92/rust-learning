300da7a84f09153304ff5b867cf7d65ec068e487
bleh92 <kumarvijay65553@gmail.com>
2024-04-28 16:09:27 -0700

    Added the feature allowing user to know if the guess they made is small, big or equal to the random number generated by the code

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -6,22 +6,22 @@ fn main() {
     println!("Guess the number!");
     
 
-    println!("Please enter the number: ");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    //println!("Secret number is {}", secret_number);
     let mut guess = String::new();
+    //println!("Secret number is {}", secret_number);
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("U  haved guessed {}", guess);
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Guess is too small");
-        Ordering::Greater => println!("Guess is too big");
-        Ordering::Equal => println!("You win");
+     let mut guess: u32 = guess.trim().parse().expect("Please enter a number: ");
 
+    println!("You haved guessed {}", guess);
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Guess is too small"),
+        Ordering::Greater => println!("Guess is too big"),
+        Ordering::Equal => println!("You win"),
+    }
 
 }
