146cb528ebeee4c624d68633776f884cb24b9024
bleh92 <kumarvijay65553@gmail.com>
2024-04-28 16:30:03 -0700

    Added a loop to keep asking the user to enter the number until they guess correctly, also added a 'break' function to exit once the user gets the correct answer

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -7,21 +7,32 @@ fn main() {
     
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    
 
-    let mut guess = String::new();
-    //println!("Secret number is {}", secret_number);
+    loop {
+        println!("Please enter your guess");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-     let mut guess: u32 = guess.trim().parse().expect("Please enter a number: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the input");
+        let mut guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+            
+        };
 
-    println!("You haved guessed {}", guess);
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Guess is too small"),
-        Ordering::Greater => println!("Guess is too big"),
-        Ordering::Equal => println!("You win"),
+        println!("You have guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+
+            Ordering::Less => println!("The number is too small"),
+            Ordering::Greater => println!("The number is too big"),
+            Ordering::Equal => {
+                println!("BINGOOOO!!! "); 
+                break;
+            }
+        }
     }
-
 }
